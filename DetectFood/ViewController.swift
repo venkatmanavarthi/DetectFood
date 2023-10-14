ce7654603c681a18af012fbb067c8bce635459b3
venkatmanavarthi <manavarthivenkat@icloud.com>
2023-10-13 20:31:07 -0500

    Initial Commit

---
+++ DetectFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetectFood
+//
+//  Created by Venkat Rao Manavarthi on 10/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
